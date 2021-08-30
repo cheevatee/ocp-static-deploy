609f59249a024f313c280cc8376759a3cfded8a6
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-08-30 22:08:17 +0700

    Update sed replace command

--- add_ocp_static_worker_nodes.sh
+++ add_ocp_static_worker_nodes.sh
@@ -4,4 +4,5 @@ export NEW_IGNITION_FILE=/var/www/html/add_worker_nodes.ign
 export MCS=api-int.$CLUSTER_DOMAIN:22623
 export API_INIT_CERT=$(openssl s_client -connect $MCS -showcerts </dev/null 2>/dev/null|openssl x509 -outform PEM | base64 --wrap=0)
 cp $ORI_IGNITION_FILE $NEW_IGNITION_FILE
-sed --regexp-extended --in-place=.backup "s%base64,[^,]+%base64,$(echo $API_INIT_CERT)\"%" $NEW_IGNITION_FILE
+#sed --regexp-extended --in-place=.backup "s%base64,[^,]+%base64,$(echo $API_INIT_CERT)\"%" $NEW_IGNITION_FILE
+sed --regexp-extended --in-place=.backup "s%base64,[a-zA-Z0-9+\/=]+%base64,$(echo $API_INIT_CERT)%" $NEW_IGNITION_FILE
