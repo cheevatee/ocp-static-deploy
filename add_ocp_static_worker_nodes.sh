b2532efb3c9c208b3b266e5373c43773332193d0
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-09-01 19:11:13 +0700

    Update parameter ORI_IGNITION_FILE
    ORI_IGNITION_FILE=/var/www/html/worker01.ign to /var/www/html/worker.ign

--- add_ocp_static_worker_nodes.sh
+++ add_ocp_static_worker_nodes.sh
@@ -1,5 +1,5 @@
 export CLUSTER_DOMAIN=ocp4.example.com
-export ORI_IGNITION_FILE=/var/www/html/worker01.ign
+export ORI_IGNITION_FILE=/var/www/html/worker.ign
 export NEW_IGNITION_FILE=/var/www/html/add_worker_nodes.ign
 export MCS=api-int.$CLUSTER_DOMAIN:22623
 export API_INIT_CERT=$(openssl s_client -connect $MCS -showcerts </dev/null 2>/dev/null|openssl x509 -outform PEM | base64 --wrap=0)
