05b6c3a4fd3cb2dce589f75fe3ca935cd8872908
cheevatee <38664398+cheevatee@users.noreply.github.com>
2021-09-01 21:12:36 +0700

    Create setup_pxe_boot.sh

---
+++ setup_pxe_boot.sh
@@ -0,0 +1,6 @@
+mkdir -p /var/lib/tftpboot/pxelinux.cfg
+cp /usr/share/syslinux/vesamenu.c32 /var/lib/tftpboot/
+cp /usr/share/syslinux/pxelinux.0 /var/lib/tftpboot/
+cp /usr/share/syslinux/ldlinux.c32 /var/lib/tftpboot/
+cp /usr/share/syslinux/libcom32.c32 /var/lib/tftpboot/
+cp /usr/share/syslinux/libutil.c32 /var/lib/tftpboot/
